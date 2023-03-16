8598b4d496f4d3ab65f4d0a496468a4fa559ccba
Ali Sherief <ali@notatether.com>
2023-03-16 10:03:47 +0000

    add support for Ravencoin

--- pywallet/utils/bip32.py
+++ pywallet/utils/bip32.py
@@ -669,6 +669,10 @@ class Wallet(object):
             response = QtumMainNet
         elif network == 'qtum_testnet' or network == 'QTUMTEST':
             response = QtumTestNet
+        elif network == 'raven' or network == 'RVN':
+            response = RavenMainNet
+        elif network == 'raven_testnet' or network == 'RVNTEST':
+            response = RavenTestNet
         elif network == 'bitcore' or network == 'BTX':
             response = BitcoreMainNet
         elif network == 'bitcore_testnet' or network == 'BTX_TESTNET':
