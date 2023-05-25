29a933db3aeca88e21780d669a62d414efbba2c5
Ali Sherief <ali@notatether.com>
2023-05-25 12:39:59 +0000

    Cut the list of supported versions due to deps

--- setup.py
+++ setup.py
@@ -41,11 +41,6 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
 
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
