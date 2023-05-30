8acb2738cc5015d6ab8e49c42c27b8907b7623fe
Ali Sherief <ali@notatether.com>
2023-05-30 12:46:53 +0000

    fix BIP84 test cases

--- tests/test_zpywallet.py
+++ tests/test_zpywallet.py
@@ -24,6 +24,9 @@ class TestZPyWallet(unittest.TestCase):
         hdw = wallet.create_wallet_json()
         assert hdw
 
+        mne = wallet.generate_mnemonic(strength=128)
+        assert mne.count(' ') == 11
+
         hdw = wallet.create_wallet(mnemonic="nature story debris circle decrease post "
                                     "gesture cute burger chef silent grief",
                                     network="btc")
@@ -33,7 +36,6 @@ class TestZPyWallet(unittest.TestCase):
     def test_001_bip32(self):
         """Test BIP32 confirmance."""
         
-        # Test vector 1
         hdw = Wallet.from_master_seed(binascii.unhexlify("000102030405060708090a0b0c0d0e0f"), network="btc")
         assert hdw.serialize_b58(private=False) == "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8"
         assert hdw.serialize_b58(private=True) == "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi"
@@ -58,7 +60,8 @@ class TestZPyWallet(unittest.TestCase):
         assert child5.serialize_b58(private=False) == "xpub6H1LXWLaKsWFhvm6RVpEL9P4KfRZSW7abD2ttkWP3SSQvnyA8FSVqNTEcYFgJS2UaFcxupHiYkro49S8yGasTvXEYBVPamhGW6cFJodrTHy"
         assert child5.serialize_b58(private=True) == "xprvA41z7zogVVwxVSgdKUHDy1SKmdb533PjDz7J6N6mV6uS3ze1ai8FHa8kmHScGpWmj4WggLyQjgPie1rFSruoUihUZREPSL39UNdE3BBDu76"
 
-        # Test vector 2
+    def test_002_bip32(self):
+        """Test BIP32 confirmance."""
         hdw = Wallet.from_master_seed(binascii.unhexlify("fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542"), network="btc")
         assert hdw.serialize_b58(private=False) == "xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB"
         assert hdw.serialize_b58(private=True) == "xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U"
@@ -83,7 +86,8 @@ class TestZPyWallet(unittest.TestCase):
         assert child5.serialize_b58(private=False) == "xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt"
         assert child5.serialize_b58(private=True) == "xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j"
 
-        # Test vector 3 - retention of leading zeroes
+    def test_003_bip32(self):
+        """Test BIP32 confirmance - retention of leading zeroes."""
         hdw = Wallet.from_master_seed(binascii.unhexlify("4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be"), network="btc")
         assert hdw.serialize_b58(private=False) == "xpub661MyMwAqRbcEZVB4dScxMAdx6d4nFc9nvyvH3v4gJL378CSRZiYmhRoP7mBy6gSPSCYk6SzXPTf3ND1cZAceL7SfJ1Z3GC8vBgp2epUt13"
         assert hdw.serialize_b58(private=True) == "xprv9s21ZrQH143K25QhxbucbDDuQ4naNntJRi4KUfWT7xo4EKsHt2QJDu7KXp1A3u7Bi1j8ph3EGsZ9Xvz9dGuVrtHHs7pXeTzjuxBrCmmhgC6"
@@ -92,7 +96,8 @@ class TestZPyWallet(unittest.TestCase):
         assert child1.serialize_b58(private=False) == "xpub68NZiKmJWnxxS6aaHmn81bvJeTESw724CRDs6HbuccFQN9Ku14VQrADWgqbhhTHBaohPX4CjNLf9fq9MYo6oDaPPLPxSb7gwQN3ih19Zm4Y"
         assert child1.serialize_b58(private=True) == "xprv9uPDJpEQgRQfDcW7BkF7eTya6RPxXeJCqCJGHuCJ4GiRVLzkTXBAJMu2qaMWPrS7AANYqdq6vcBcBUdJCVVFceUvJFjaPdGZ2y9WACViL4L"
 
-        # Test vector 4 - retention of leading zeroes
+    def test_004_bip32(self):
+        """Test BIP32 confirmance - retention of leading zeroes."""
         hdw = Wallet.from_master_seed(binascii.unhexlify("3ddd5602285899a946114506157c7997e5444528f3003f6134712147db19b678"), network="btc")
         assert hdw.serialize_b58(private=False) == "xpub661MyMwAqRbcGczjuMoRm6dXaLDEhW1u34gKenbeYqAix21mdUKJyuyu5F1rzYGVxyL6tmgBUAEPrEz92mBXjByMRiJdba9wpnN37RLLAXa"
         assert hdw.serialize_b58(private=True) == "xprv9s21ZrQH143K48vGoLGRPxgo2JNkJ3J3fqkirQC2zVdk5Dgd5w14S7fRDyHH4dWNHUgkvsvNDCkvAwcSHNAQwhwgNMgZhLtQC63zxwhQmRv"
@@ -105,7 +110,8 @@ class TestZPyWallet(unittest.TestCase):
         assert child2.serialize_b58(private=False) == "xpub6BJA1jSqiukeaesWfxe6sNK9CCGaujFFSJLomWHprUL9DePQ4JDkM5d88n49sMGJxrhpjazuXYWdMf17C9T5XnxkopaeS7jGk1GyyVziaMt"
         assert child2.serialize_b58(private=True) == "xprv9xJocDuwtYCMNAo3Zw76WENQeAS6WGXQ55RCy7tDJ8oALr4FWkuVoHJeHVAcAqiZLE7Je3vZJHxspZdFHfnBEjHqU5hG1Jaj32dVoS6XLT1"
 
-        # Test vector 3 - invalid HD keys
+    def test_005_bip32(self):
+        """Test BIP32 confirmance - invalid HD keys."""
         with self.assertRaises(IncompatibleNetworkException):
             Wallet.deserialize("xpub661MyMwAqRbcEYS8w7XLSVeEsBXy79zSzH1J8vCdxAZningWLdN3zgtU6LBpB85b3D2yc8sfvZU521AAwdZafEz7mnzBBsz4wKY5fTtTQBm")
         
@@ -153,3 +159,47 @@ class TestZPyWallet(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             Wallet.deserialize("xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHL")
+
+    def test_006_bip84(self):
+        """Test BIP84 confirmance"""
+        mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+        hdw = Wallet.from_mnemonic(mnemonic)
+        assert hdw.serialize_b58(private=True, segwit=True) == \
+            "zprvAWgYBBk7JR8Gjrh4UJQ2uJdG1r3WNRRfURiABBE3RvMXYSrRJL62XuezvGdPvG6GFBZduosCc1YP5wixPox7zhZLfiUm8aunE96BBa4Kei5"
+        assert hdw.serialize_b58(private=False, segwit=True) == \
+            "zpub6jftahH18ngZxLmXaKw3GSZzZsszmt9WqedkyZdezFtWRFBZqsQH5hyUmb4pCEeZGmVfQuP5bedXTB8is6fTv19U1GQRyQUKQGUTzyHACMF"
+
+        # Account 0, root = m/84'/0'/0'
+        hdw1 = hdw.get_child_for_path("m/84'/0'/0'")
+        assert hdw1.serialize_b58(private=True, segwit=True) == \
+            "zprvAdG4iTXWBoARxkkzNpNh8r6Qag3irQB8PzEMkAFeTRXxHpbF9z4QgEvBRmfvqWvGp42t42nvgGpNgYSJA9iefm1yYNZKEm7z6qUWCroSQnE"
+        assert hdw1.serialize_b58(private=False, segwit=True) == \
+            "zpub6rFR7y4Q2AijBEqTUquhVz398htDFrtymD9xYYfG1m4wAcvPhXNfE3EfH1r1ADqtfSdVCToUG868RvUUkgDKf31mGDtKsAYz2oz2AGutZYs"
+
+        # Account 0, first receiving address = m/84'/0'/0'/0/0
+        hdw2 = hdw.get_child_for_path("m/84'/0'/0'/0/0")
+        assert hdw2.private_key.to_wif(compressed=True) == \
+            "KyZpNDKnfs94vbrwhJneDi77V6jF64PWPF8x5cdJb8ifgg2DUc9d"
+        assert hdw2.public_key.to_hex(compressed=True) == \
+            "0330d54fd0dd420a6e5f8d3624f5f3482cae350f79d5f0753bf5beef9c2d91af3c"
+        assert hdw2.public_key.bech32_address(compressed=True, witness_version=0) == \
+            "bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu"
+
+        # Account 0, second receiving address = m/84'/0'/0'/0/1
+        hdw3 = hdw.get_child_for_path("m/84'/0'/0'/0/1")
+        assert hdw3.private_key.to_wif(compressed=True) == \
+            "Kxpf5b8p3qX56DKEe5NqWbNUP9MnqoRFzZwHRtsFqhzuvUJsYZCy"
+        assert hdw3.public_key.to_hex(compressed=True) == \
+            "03e775fd51f0dfb8cd865d9ff1cca2a158cf651fe997fdc9fee9c1d3b5e995ea77"
+        assert hdw3.public_key.bech32_address(compressed=True, witness_version=0) == \
+            "bc1qnjg0jd8228aq7egyzacy8cys3knf9xvrerkf9g"
+
+        # Account 0, first change address = m/84'/0'/0'/1/0
+        hdw4 = hdw.get_child_for_path("m/84'/0'/0'/1/0")
+        assert hdw4.private_key.to_wif(compressed=True) == \
+            "KxuoxufJL5csa1Wieb2kp29VNdn92Us8CoaUG3aGtPtcF3AzeXvF"
+        assert hdw4.public_key.to_hex(compressed=True) == \
+            "03025324888e429ab8e3dbaf1f7802648b9cd01e9b418485c5fa4c1b9b5700e1a6"
+        assert hdw4.public_key.bech32_address(compressed=True, witness_version=0) == \
+            "bc1q8c6fshw2dlwun7ekn9qwf37cu2rn755upcp6el"
+
