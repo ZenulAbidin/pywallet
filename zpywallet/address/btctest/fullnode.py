df863669cbca7bbb757234f29f89be261fe4e2f7
Ali Sherief <ali@notatether.com>
2023-07-25 10:23:36 +0000

    Improve support for the fullnodes

--- zpywallet/address/btctest/fullnode.py
+++ zpywallet/address/btctest/fullnode.py
@@ -1,8 +1,10 @@
 import random
 import requests
+from datetime import datetime
 
 from ...errors import NetworkException
 from ...utils.descriptors import descsum_create_only
+from ...transactions.decode import parse_transaction_simple
 
 def to_descriptor(address):
     ad = f"addr({address})"
@@ -29,25 +31,17 @@ def to_extpub_descriptor(expub, path):
 
 class BitcoinRPCClient:
     """Address querying class for Bitcoin full nodes utilizing descriptors.
-       Requires node version v0.17.0 or later.
+       Requires node version v0.17.0 or later running with -txindex.
     """
-
-    @staticmethod
-    def _clean_utxos(element):
-        new_element = {
-            "txid": element["txid"],
-            "index": element["vout"],
-            "amount": element["amount"], # should be in the base value, eg. BTC, LTC, not sats or gwei
-            "height": element["height"],
-            "address": element["address"]
-        }
-        return new_element
     
     # Not static because we need to make calls to fetch input transactions.
     def _clean_tx(self, element):
         new_element = {}
         new_element['txid'] = element['txid']
-        new_element['height'] = element['block_height']
+        if 'blockheight' in element.keys():
+            new_element['height'] = element['blockheight']
+        else:
+            new_element['height'] = None
         new_element['timestamp'] = element['time']
         element = element['decoded']
 
@@ -55,18 +49,29 @@ class BitcoinRPCClient:
         new_element['outputs'] = []
         for vin in element['vin']:
             txinput = {}
-            txinput['txid'] = vin['txid']
-            txinput['index'] = vin['vout']
-            # To fill in the amount, we have to get the other transaction id
-            res = self._send_rpc_request('gettransaction', params=[vin['txid'], True, True], as_wallet=True)
-            txinput['amount'] = res['result']['decoded']['vout'][vin['vout']]['value']
+            if 'txid' in vin.keys():
+                txinput['txid'] = vin['txid']
+                txinput['index'] = vin['vout']
+                # To fill in the amount, we have to get the other transaction id
+                # But only if we're not parsing a coinbase transaction
+                res = self._send_rpc_request('getrawtransaction', params=[vin['txid']])
+                rawtx = res['result']
+                fine_rawtx, _ = parse_transaction_simple(rawtx)
+                txinput['amount'] = fine_rawtx["outputs"][txinput['index']]["value"] / 1e8
+            else:
+                txinput['txid'] = None
+                txinput['index'] = None
+                txinput['amount'] = 0
 
             new_element['inputs'].append(txinput)
         for vout in element['vout']:
             txoutput = {}
             txoutput['amount'] = vout['value']
             txoutput['index'] = vout['n']
-            txoutput['address'] = vout['scriptPubKey']['address']
+            if 'address' in vout['scriptPubKey']:
+                txoutput['address'] = vout['scriptPubKey']['address']
+            else:
+                txoutput['address'] = None
             new_element['outputs'].append(txoutput)
         
         # Now we must calculate the total fee
@@ -79,14 +84,20 @@ class BitcoinRPCClient:
         new_element['fee_metric'] = 'vbyte'
         return new_element
 
-    def __init__(self, rpc_url, rpc_user, rpc_password, client_number=0, user_id=0, block_height=0, max_tx_at_once=1000):
+    def __init__(self, addresses, rpc_url, rpc_user, rpc_password, client_number=0, user_id=0, last_update=0, max_tx_at_once=1000):
         self.rpc_url = rpc_url
         self.rpc_user = rpc_user
         self.rpc_password = rpc_password
         self.client_number = client_number
         self.user_id = user_id
-        self.block_height = block_height
         self.max_tx_at_once = max_tx_at_once
+        
+        self.transactions = []
+        self.addresses = addresses
+        self.last_update = last_update
+        self.height = self.get_block_height()
+        self._load_addresses()
+        self.transactions = [*self._get_transaction_history()]
     
     def _send_rpc_request(self, method, params=None, as_wallet=False):
         payload = {
@@ -102,27 +113,27 @@ class BitcoinRPCClient:
         return response.json()
     
     def get_block_height(self):
-        # set self.block_height here
         response = self._send_rpc_request('getblockchaininfo')
         return response['result']['blocks']
 
-    def load_addresses(self, addresses):
+    def _load_addresses(self):
         # Create a new temporary wallet
-        self._send_rpc_request('createwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
+        self._send_rpc_request('createwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}", True])
         
-        block_height = self.block_height
+        block_height = self.height
         block_height = min(block_height, self.get_block_height())
 
 
-        params = [*map(lambda at: {"desc": to_descriptor(at), "timestamp": block_height}, addresses)]
+        params = [*map(lambda at: {"desc": to_descriptor(at), "timestamp": self.last_update}, self.addresses)]
 
         try:
             # Load the temporary wallet
             self._send_rpc_request('loadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
+
+            self.last_update = datetime.now().timestamp()
             
             # Import the address into the wallet
             self._send_rpc_request('importdescriptors', params=[params], as_wallet=True)
-            self.block_height = self.get_block_height()
         
         finally:
             # Unload the temporary wallet
@@ -144,29 +155,55 @@ class BitcoinRPCClient:
         for utxo in utxos:
             total_balance += utxo["amount"]
             # Careful: Block height #0 is the Genesis block - don't want to exclude that.
+            # (Not that it returns it ever though!)
             if utxo["height"] is not None:
                 confirmed_balance += utxo["amount"]
         return total_balance, confirmed_balance
-
-    def get_utxos(self):
-        try:
-            # Load the temporary wallet
-            self._send_rpc_request('loadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
-            
-                # Get UTXOs for the imported address
-            result = self._send_rpc_request('listunspent', params=[0, 99999999], as_wallet=True)
-            utxos = BitcoinRPCClient._clean_utxos(result['result'])
-            return utxos
         
-        finally:
-            # Unload and delete the temporary wallet
-            self._send_rpc_request('unloadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
+    def get_utxos(self):
+        self.height = self.get_block_height()
+        # Transactions are generated in reverse order
+        utxos = []
+        for i in range(len(self.transactions)-1, -1, -1):
+            for utxo in [u for u in utxos]:
+                # Check if any utxo has been spent in this transaction
+                for vin in self.transactions[i]["inputs"]:
+                    if vin["txid"] == utxo["txid"] and vin["index"] == utxo["index"]:
+                        # Spent
+                        utxos.remove(utxo)
+            for out in self.transactions[i]["outputs"]:
+                if out["address"] in self.addresses:
+                    utxo = {}
+                    utxo["address"] = out["address"]
+                    utxo["txid"] = self.transactions[i]["txid"]
+                    utxo["index"] = out["index"]
+                    utxo["amount"] = out["amount"]
+                    utxo["height"] = self.transactions[i]["height"]
+                    utxos.append(utxo)
+        return utxos
     
     def get_transaction_history(self):
+        """
+        Retrieves the transaction history of the Bitcoin address from cached data augmented with network data.
+        Does not include Genesis blocks.
 
-        # We need to get info of all addresses attached to a transaction, not just our own.
-        # This may require a node with txindex=1 switched on.
-        
+        Returns:
+            list: A list of dictionaries representing the transaction history.
+
+        Raises:
+            Exception: If the RPC request fails or the transaction history cannot be retrieved.
+        """
+        if len(self.transactions) == 0:
+            self.transactions = [*self.get_transaction_history()]
+        else:
+            # First element is the most recent transactions
+            txhash = self.transactions[0]["txid"]
+            txs = [*self._get_transaction_history(txhash)]
+            txs.extend(self.transactions)
+            self.transactions = txs
+            del txs
+    
+    def _get_transaction_history(self, txhash=None):
         try:
             # Load the temporary wallet
             self._send_rpc_request('loadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
@@ -174,44 +211,33 @@ class BitcoinRPCClient:
              # Get the number of transactions
             total_transactions = self._send_rpc_request('getwalletinfo', as_wallet=True)['result']['txcount']
 
-            skip_later_txs = total_transactions - self.max_tx_at_once
-            while skip_later_txs > -self.max_tx_at_once:
+            skip_later_txs = 0
+            while skip_later_txs < total_transactions:
 
-                # If the subtraction gave us a negative skip offset, just make it zero to get the last most recent few transactions
-                skip_later_txs = max(skip_later_txs, 0)
                 # listtransactions by default return only 10 most recent transactions)
-                # However, we cannot simply just make this a huge number, otherwise we will run out of memory if the wallet
-                # has too many (in the hundreds of thousands) transactions.
                 result = self._send_rpc_request('listtransactions', params=["*", self.max_tx_at_once, skip_later_txs, True], as_wallet=True)
                 transactions = result['result']
                 
                 # Extract the transaction IDs so we can query them verbosely
                 # because the tx doesn't include info on other addresses
-                txids = []
                 for tx in transactions:
-                    txids.append(tx["txid"])
-
-                # Call gettransaction for each txid (requires txindex=1 set by the
-                transactions = []
-                for txid in txids:
+                    txid = tx['txid']
+                    if txid == txhash:
+                        return
                     result = self._send_rpc_request('gettransaction', params=[txid, True, True], as_wallet=True)
-                    txs = result['result']
-                    
-                    for tx in txs:
-                        transactions.append(self._clean_tx(tx))
+                    result_tx = result['result']
+                    yield self._clean_tx(result_tx)
             
+
                 # Get the number of transactions in case new, incoming txs arrived.
+                # We will just skip those txs and they can be received in the next method invocation.
                 total_transactions_new = self._send_rpc_request('getwalletinfo', as_wallet=True)['result']['txcount']
                 if total_transactions_new > total_transactions:
-                    # Transactions we haven't listed yet have been pushed into our current numerical range, so push
-                    # the skipped later transactions count forward,
                     skip_later_txs += total_transactions_new - total_transactions
                     total_transactions = total_transactions_new
 
                 # And finally prepare to process next most recent batch of transactions
-                skip_later_txs -= self.max_tx_at_once
-
-            return transactions
+                skip_later_txs += self.max_tx_at_once
         
         finally:
             # Unload and delete the temporary wallet
--- zpywallet/address/ltc/fullnode.py
+++ zpywallet/address/ltc/fullnode.py
@@ -1,28 +1,23 @@
 import random
 import requests
+from datetime import datetime
 
-from zpywallet.errors import NetworkException
+from ...transactions.decode import parse_transaction_simple
 
 
-# WARNING: DOES NOT WORK WITH DOGECOIN.
-class CryptoRPCClient:
-
-    @staticmethod
-    def _clean_utxos(element):
-        new_element = {
-            "txid": element["txid"],
-            "index": element["vout"],
-            "amount": element["amount"], # should be in the base value, eg. BTC, LTC, not sats or gwei
-            "height": element["height"],
-            "address": element["address"]
-        }
-        return new_element
+class LitecoinRPCClient:
+    """Address querying class for Litecoin full nodes utilizing descriptors.
+       Requires node with multi-wallet support and -txindex.
+    """
     
     # Not static because we need to make calls to fetch input transactions.
     def _clean_tx(self, element):
         new_element = {}
         new_element['txid'] = element['txid']
-        new_element['height'] = element['block_height']
+        if 'blockheight' in element.keys():
+            new_element['height'] = element['blockheight']
+        else:
+            new_element['height'] = None
         new_element['timestamp'] = element['time']
         element = element['decoded']
 
@@ -30,32 +25,35 @@ class CryptoRPCClient:
         new_element['outputs'] = []
         for vin in element['vin']:
             txinput = {}
-            txinput['txid'] = vin['txid']
-            txinput['index'] = vin['vout']
-            # To fill in the amount, we have to get the other transaction id
-            res = self._send_rpc_request('gettransaction', params=[vin['txid'], True, True], as_wallet=True)
-            txinput['amount'] = res['result']['decoded']['vout'][vin['vout']]['value']
+            if 'txid' in vin.keys():
+                txinput['txid'] = vin['txid']
+                txinput['index'] = vin['vout']
+                # To fill in the amount, we have to get the other transaction id
+                # But only if we're not parsing a coinbase transaction
+                res = self._send_rpc_request('getrawtransaction', params=[vin['txid']])
+                rawtx = res['result']
+                fine_rawtx, _ = parse_transaction_simple(rawtx)
+                txinput['amount'] = fine_rawtx["outputs"][txinput['index']]["value"] / 1e8
+            else:
+                txinput['txid'] = None
+                txinput['index'] = None
+                txinput['amount'] = 0
 
             new_element['inputs'].append(txinput)
         for vout in element['vout']:
             txoutput = {}
             txoutput['amount'] = vout['value']
             txoutput['index'] = vout['n']
-            if 'address' in vout['scriptPubKey'].keys():
+            if 'address' in vout['scriptPubKey']:
                 txoutput['address'] = vout['scriptPubKey']['address']
-            elif 'addresses' in vout['scriptPubKey'].keys() and len(vout['scriptPubKey']['addresses']) > 0:
-                # Multiple addresses may be defined if this is a multisig output. We don't support multisig
-                # wallets or addresses, so we will always just return the first one.
-                # Other than that it's usually just 1 address in a list, or at least that's how eg. LTC returns it.
-                txoutput['address'] = vout['scriptPubKey']['addresses'][0]
             else:
-                # Address not defined for this output. (for example, if it's a rare P2PK output)
                 txoutput['address'] = None
             new_element['outputs'].append(txoutput)
         
         # Now we must calculate the total fee
         total_inputs = sum([a['amount'] for a in new_element['inputs']])
         total_outputs = sum([a['amount'] for a in new_element['outputs']])
+
         new_element['total_fee'] = (total_inputs - total_outputs) / 1e8
 
         # vbytes will be the default transaction metric for networks that support segwit,
@@ -66,19 +64,23 @@ class CryptoRPCClient:
         else:
             new_element['fee'] = (total_inputs - total_outputs) / element['size']
             new_element['fee'] = 'byte'
-        
+
         return new_element
 
-
-
-    def __init__(self, rpc_url, rpc_user, rpc_password, client_number=0, user_id=0, block_height=0, max_tx_at_once=1000):
+    def __init__(self, addresses, rpc_url, rpc_user, rpc_password, client_number=0, user_id=0, last_update=0, max_tx_at_once=1000):
         self.rpc_url = rpc_url
         self.rpc_user = rpc_user
         self.rpc_password = rpc_password
         self.client_number = client_number
         self.user_id = user_id
-        self.block_height = block_height
         self.max_tx_at_once = max_tx_at_once
+        
+        self.transactions = []
+        self.addresses = addresses
+        self.last_update = last_update
+        self.height = self.get_block_height()
+        self._load_addresses()
+        self.transactions = [*self._get_transaction_history()]
     
     def _send_rpc_request(self, method, params=None, as_wallet=False):
         payload = {
@@ -87,24 +89,25 @@ class CryptoRPCClient:
             'jsonrpc': '2.0',
             'id': random.randint(1, 999999)
         }
+        # Full nodes wallet RPC requests are notoriously slow if there are many transactions in the node.
         response = requests.post(f"{self.rpc_url}/wallet/zpywallet_{self.client_number}_{self.user_id}" if as_wallet \
                                  else self.rpc_url, auth=(self.rpc_user, self.rpc_password), json=payload, timeout=86400)
         #response.raise_for_status()
         return response.json()
     
     def get_block_height(self):
-        # set self.block_height here
         response = self._send_rpc_request('getblockchaininfo')
         return response['result']['blocks']
 
-    def load_addresses(self, addresses):
+    def _load_addresses(self):
         # Create a new temporary wallet
-        self._send_rpc_request('createwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
+        self._send_rpc_request('createwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}", True])
         
-        block_height = self.block_height
+        block_height = self.height
         block_height = min(block_height, self.get_block_height())
 
-        params = [*map(lambda at: {"scriptPubKey": {"address": at}, "timestamp": block_height}, addresses)]
+
+        params = [*map(lambda at: {"scriptPubKey": {"address": at}, "timestamp": self.last_update}, self.addresses)]
 
         try:
             # Load the temporary wallet
@@ -112,18 +115,19 @@ class CryptoRPCClient:
             
             # Import the address into the wallet
             self._send_rpc_request('importmulti', params=[params], as_wallet=True)
-            self.block_height = self.get_block_height()
-        
+            
+            self.last_update = datetime.now().timestamp()
+            
         finally:
             # Unload the temporary wallet
             self._send_rpc_request('unloadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
     
     def get_balance(self):
         """
-        Retrieves the balance of the Bitcoin address.
+        Retrieves the balance of the Litecoin address.
 
         Returns:
-            float: The balance of the Bitcoin address in BTC.
+            float: The balance of the Litecoin address in BTC.
 
         Raises:
             Exception: If the API request fails or the address balance cannot be retrieved.
@@ -134,76 +138,91 @@ class CryptoRPCClient:
         for utxo in utxos:
             total_balance += utxo["amount"]
             # Careful: Block height #0 is the Genesis block - don't want to exclude that.
+            # (Not that it returns it ever though!)
             if utxo["height"] is not None:
                 confirmed_balance += utxo["amount"]
         return total_balance, confirmed_balance
-
-    def get_utxos(self):
-        try:
-            # Load the temporary wallet
-            self._send_rpc_request('loadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
-            
-                # Get UTXOs for the imported address
-            result = self._send_rpc_request('listunspent', params=[0, 99999999], as_wallet=True)
-            utxos = CryptoRPCClient._clean_utxos(result['result'])
-            
-            return utxos
         
-        finally:
-            # Unload and delete the temporary wallet
-            self._send_rpc_request('unloadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
+    def get_utxos(self):
+        self.height = self.get_block_height()
+        # Transactions are generated in reverse order
+        utxos = []
+        for i in range(len(self.transactions)-1, -1, -1):
+            for utxo in [u for u in utxos]:
+                # Check if any utxo has been spent in this transaction
+                for vin in self.transactions[i]["inputs"]:
+                    if vin["txid"] == utxo["txid"] and vin["index"] == utxo["index"]:
+                        # Spent
+                        utxos.remove(utxo)
+            for out in self.transactions[i]["outputs"]:
+                if out["address"] in self.addresses:
+                    utxo = {}
+                    utxo["address"] = out["address"]
+                    utxo["txid"] = self.transactions[i]["txid"]
+                    utxo["index"] = out["index"]
+                    utxo["amount"] = out["amount"]
+                    utxo["height"] = self.transactions[i]["height"]
+                    utxos.append(utxo)
+        return utxos
     
     def get_transaction_history(self):
+        """
+        Retrieves the transaction history of the Litecoin address from cached data augmented with network data.
+        Does not include Genesis blocks.
 
-        # We need to get info of all addresses attached to a transaction, not just our own.
-        # This may require a node with txindex=1 switched on.
-        
+        Returns:
+            list: A list of dictionaries representing the transaction history.
+
+        Raises:
+            Exception: If the RPC request fails or the transaction history cannot be retrieved.
+        """
+        if len(self.transactions) == 0:
+            self.transactions = [*self.get_transaction_history()]
+        else:
+            # First element is the most recent transactions
+            txhash = self.transactions[0]["txid"]
+            txs = [*self._get_transaction_history(txhash)]
+            txs.extend(self.transactions)
+            self.transactions = txs
+            del txs
+    
+    def _get_transaction_history(self, txhash=None):
         try:
             # Load the temporary wallet
             self._send_rpc_request('loadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
-
-            # Get the number of transactions
+            
+             # Get the number of transactions
             total_transactions = self._send_rpc_request('getwalletinfo', as_wallet=True)['result']['txcount']
 
-            skip_later_txs = total_transactions - self.max_tx_at_once
-            while skip_later_txs > -self.max_tx_at_once:
+            skip_later_txs = 0
+            while skip_later_txs < total_transactions:
 
-                # If the subtraction gave us a negative skip offset, just make it zero to get the last most recent few transactions
-                skip_later_txs = max(skip_later_txs, 0)
                 # listtransactions by default return only 10 most recent transactions)
-                # However, we cannot simply just make this a huge number, otherwise we will run out of memory if the wallet
-                # has too many (in the hundreds of thousands) transactions.
                 result = self._send_rpc_request('listtransactions', params=["*", self.max_tx_at_once, skip_later_txs, True], as_wallet=True)
                 transactions = result['result']
                 
                 # Extract the transaction IDs so we can query them verbosely
                 # because the tx doesn't include info on other addresses
-                txids = []
                 for tx in transactions:
-                    txids.append(tx["txid"])
-
-                # Call gettransaction for each txid (requires txindex=1 set by the
-                transactions = []
-                for txid in txids:
+                    txid = tx['txid']
+                    if txid == txhash:
+                        return
                     result = self._send_rpc_request('gettransaction', params=[txid, True, True], as_wallet=True)
-                    txs = result['result']
-                    
-                    for tx in txs:
-                        transactions.append(self._clean_tx(tx))
+                    result_tx = result['result']
+                    yield self._clean_tx(result_tx)
             
+
                 # Get the number of transactions in case new, incoming txs arrived.
+                # We will just skip those txs and they can be received in the next method invocation.
                 total_transactions_new = self._send_rpc_request('getwalletinfo', as_wallet=True)['result']['txcount']
                 if total_transactions_new > total_transactions:
-                    # Transactions we haven't listed yet have been pushed into our current numerical range, so push
-                    # the skipped later transactions count forward,
                     skip_later_txs += total_transactions_new - total_transactions
                     total_transactions = total_transactions_new
 
                 # And finally prepare to process next most recent batch of transactions
-                skip_later_txs -= self.max_tx_at_once
-
-            return transactions
+                skip_later_txs += self.max_tx_at_once
         
         finally:
             # Unload and delete the temporary wallet
             self._send_rpc_request('unloadwallet', params=[f"zpywallet_{self.client_number}_{self.user_id}"])
+
