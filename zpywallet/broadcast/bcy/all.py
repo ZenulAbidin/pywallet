b9e8bc50873a1edc42df5423caaf532c7c090289
Ali Sherief <ali@notatether.com>
2024-01-07 13:59:42 +0000

    fix BCY broadcasting

--- zpywallet/broadcast/bcy/all.py
+++ zpywallet/broadcast/bcy/all.py
@@ -1,5 +1,9 @@
+import binascii
 import hashlib
 from .blockcypher import *
+
+def tx_hash_bcy(raw_transaction_hex):
+    return binascii.hexlify(hashlib.sha256(hashlib.sha256(raw_transaction_hex).digest()).digest())
 
 def broadcast_transaction_bcy(raw_transaction_hex: bytes):
     errors = []
@@ -9,4 +13,4 @@ def broadcast_transaction_bcy(raw_transaction_hex: bytes):
     except NetworkException as e:
         errors.append(e)
     
-    return hashlib.sha256(hashlib.sha256(raw_transaction_hex).digest()).digest(), errors
+    return errors
