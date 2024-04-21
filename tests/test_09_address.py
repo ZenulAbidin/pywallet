06c719165d9b61ed3a77f539d0bd61e93cc49d28
Ali Sherief <ali@notatether.com>
2024-04-21 08:16:32 +0000

    update test cases

--- tests/test_09_address.py
+++ tests/test_09_address.py
@@ -4,12 +4,7 @@
 """Tests for address transaction, balance, and UTXO fetcher."""
 
 import unittest
-from zpywallet.address.btc import BitcoinAddress
-from zpywallet.address.btctest import BitcoinTestAddress
-from zpywallet.address.dash import DashAddress
-from zpywallet.address.doge import DogecoinAddress
-from zpywallet.address.eth import EthereumAddress
-from zpywallet.address.ltc import LitecoinAddress
+from zpywallet.address import CryptoClient
 from zpywallet.errors import NetworkException
 
 
@@ -22,81 +17,87 @@ class TestAddress(unittest.TestCase):
 
     def test_000_btc_address(self):
         """Test fetching Bitcoin addresses."""
-        b = BitcoinAddress(
+        b = CryptoClient(
             [
                 "3KzZceAGsA7HRxFzgbZxVJMAV9TJa8o97V",
                 "bc1plytzh6jqwltfq6l0ujt5ucz9csrlff4rfnxwmy3tkepkeyj3y2gskcf48c",
             ],
-            fast_mode=True,
+            coin="BTC",
+            chain="main",
         )
         try:
 
             b.get_transaction_history()
             b.get_utxos()
             b.get_balance()
-        except NetworkException as e:
+        except NetworkException:
             pass
 
     def test_001_btctest_address(self):
         """Test fetching Bitcoin testnet addressses"""
-        b = BitcoinTestAddress(
+        b = CryptoClient(
             [
                 "2NDNwoqdNvJ2jkBD8B6VVxNntpuKR4tkTSz",
                 "tb1qtnl457a54cz5gq0zgyf4n2xt7n9uuqxzs8jwrp",
             ],
-            fast_mode=True,
+            coin="BTC",
+            chain="TEST",
         )
         try:
 
             b.get_transaction_history()
             b.get_utxos()
             b.get_balance()
-        except NetworkException as e:
+        except NetworkException:
             pass
 
     def test_002_dash_address(self):
         """Test fetching Dash addressses"""
-        b = DashAddress(["XbzLCqAv8rkYmky6uEsxibHRUbHZU2XCKg"], fast_mode=True)
+        b = CryptoClient(
+            ["XbzLCqAv8rkYmky6uEsxibHRUbHZU2XCKg"], coin="DASH", chain="main"
+        )
         try:
 
             b.get_transaction_history()
             b.get_utxos()
             b.get_balance()
-        except NetworkException as e:
+        except NetworkException:
             pass
 
     def test_003_doge_address(self):
         """Test fetching Dogecoin addressses"""
-        b = DogecoinAddress(["D8xCRT245ax9TJVDfYZ1ErLTMtG186S9rx"], fast_mode=True)
+        b = CryptoClient(
+            ["D8xCRT245ax9TJVDfYZ1ErLTMtG186S9rx"], coin="DOGE", chain="main"
+        )
         try:
 
             b.get_transaction_history()
             b.get_utxos()
             b.get_balance()
-        except NetworkException as e:
+        except NetworkException:
             pass
 
     def test_004_eth_address(self):
         """Test fetching Ethereum addressses"""
-        b = EthereumAddress(
-            ["0x383d4669f177182f2c8c90cecd291190ea04edad"], fast_mode=True
+        b = CryptoClient(
+            ["0x383d4669f177182f2c8c90cecd291190ea04edad"], coin="ETH", chain="main"
         )
         try:
 
             # b.get_transaction_history()  # On EVM chains, this scans all blocks and takes forever.
             b.get_balance()
-        except NetworkException as e:
+        except NetworkException:
             pass
 
     def test_005_ltc_address(self):
         """Test fetching Litecoin addressses"""
-        b = LitecoinAddress(
-            ["ltc1q9pw48v23gq9d2lqcss8yaqeh7fqzu4wrt6m6nr"], fast_mode=True
+        b = CryptoClient(
+            ["ltc1q9pw48v23gq9d2lqcss8yaqeh7fqzu4wrt6m6nr"], coin="LTC", chain="main"
         )
         try:
 
             b.get_transaction_history()
             b.get_utxos()
             b.get_balance()
-        except NetworkException as e:
+        except NetworkException:
             pass
